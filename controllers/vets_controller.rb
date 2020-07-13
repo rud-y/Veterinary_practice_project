a9ac94b80bdc2b9116a5c72054a8885c1d48348c
rudy <rudolf.zachar@gmail.com>
2020-07-13 09:41:25 +0100

    Additional file structure - controllers, views created(need to refactor , not showing webpage)

---
+++ controllers/vets_controller.rb
@@ -0,0 +1,14 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('../models/vet')
+also_reload('../models/*')
+
+
+get '/vets' do 
+    @vets = Vet.find_all()
+    erb (:"vets/index")
+end
+
+# get 'vets/:id' do 
+#     @vet = Vet.find(params['id'])
+# end
